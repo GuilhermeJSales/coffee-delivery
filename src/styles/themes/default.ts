f333ff08dc91270f937b8c4757e6ec54271e73e5
GuilhermeJSales <guijsalesdev@outlook.com>
2023-08-13 21:47:17 -0300

    primeiro commit - configurando rotas e ambiente

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,24 @@
+export const defaultTheme = {
+  white: '#FFFFFF',
+
+  'yellow-100': '#F1E9C9',
+  'yellow-400': '#DBAC2C',
+  'yellow-900': '#c47F17',
+
+  'purple-100': '#EBE5F9',
+  'purple-500': '#8047F8',
+  'purple-900': '#4B2995',
+
+  title: '#272221',
+  subTitle: '#403937',
+  text: '#574f4D',
+
+  'base-label': '#8D8686',
+  'base-hover': '#D7D5D5',
+
+  'base-button': '#E6E5E5',
+  'base-input': '#EDEDED',
+  'base-card': '#F3F2F2',
+
+  background: '#FAFAFA',
+}
